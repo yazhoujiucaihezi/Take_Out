ebf3311f48529507c1ffb27c339fd15fee789b30
yazhoujiucaihezi <2464859727@qq.com>
2026-06-01 20:56:39 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,7 +1,15 @@
 package com.sky.service;
 
+import com.sky.constant.PasswordConstant;
+import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.DigestUtils;
+import sun.security.util.Password;
+
+import java.time.LocalDateTime;
 
 public interface EmployeeService {
 
@@ -12,4 +20,9 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    void save(EmployeeDTO employeeDTO);
 }
