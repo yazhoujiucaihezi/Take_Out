14e89d54b887929d9565d5b520b9d45c3fe8fc37
yazhoujiucaihezi <2464859727@qq.com>
2026-08-11 00:37:03 +0800

    导出运营数据报表

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -28,4 +29,10 @@ public interface ReportService {
     OrderReportVO getOrdersStatistics(LocalDate begin, LocalDate end);
 
     SalesTop10ReportVO getTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    void export(HttpServletResponse response);
 }
--- sky-server/src/main/java/com/sky/service/impl/WorkspaceServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/WorkspaceServiceImpl.java
@@ -45,8 +45,8 @@ public class WorkspaceServiceImpl implements WorkspaceService {
          */
 
         Map map = new HashMap();
-        map.put("begin",begin);
-        map.put("end",end);
+        map.put("beginTime",begin);
+        map.put("endTime",end);
 
         //查询总订单数
         Integer totalOrderCount = orderMapper.countByMap(map);
